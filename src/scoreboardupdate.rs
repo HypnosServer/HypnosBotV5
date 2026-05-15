8a7c7a05fdec0e03680be7f87f0d2cb507e6300f
BlueKossa <dexter@stormdal.se>
2026-05-16 01:40:16 +0200

    Optimized calcs

--- src/scoreboardupdate.rs
+++ src/scoreboardupdate.rs
@@ -24,113 +24,130 @@ impl<'a> Calc<'a> {
     }
 
     pub async fn add_player(&self, player: &str, board: &str) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players operation val calc += {} {}",
-            self.server, player, board
-        ))
-        .await
-        .unwrap();
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players operation val calc += {} {}",
+                self.server, player, board
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn add(&self, val: i64) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players add val calc {}",
-            self.server, val
-        ))
-        .await
-        .unwrap();
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players add val calc {}",
+                self.server, val
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn remove_player(&self, player: &str, board: &str) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players operation val calc -= {} {}",
-            self.server, player, board
-        ))
-        .await
-        .unwrap();
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players operation val calc -= {} {}",
+                self.server, player, board
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn remove(&self, val: i64) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players remove val calc {}",
-            self.server, val
-        ))
-        .await
-        .unwrap();
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players remove val calc {}",
+                self.server, val
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn set(&self, val: i64) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players set val calc {}",
-            self.server, val
-        ))
-        .await
-        .unwrap();
-
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players set val calc {}",
+                self.server, val
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn set_other_total(&self, board: &str) {
-        self.tx.send(format!(
-            "RCON {} scoreboard players operation Total {} = val calc",
-            self.server, board
-        ))
-        .await
-        .unwrap();
+        self.tx
+            .send(format!(
+                "RCON {} scoreboard players operation Total {} = val calc",
+                self.server, board
+            ))
+            .await
+            .unwrap();
     }
 }
 
-pub async fn scoreboard_update(ctx: &Context) {
-    let mut count = 0;
-    loop {
-        // Sleep for 1s
-        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-        let (tx, _) = ctx
-            .data
-            .read()
-            .await
-            .get::<TaurusChannel>()
-            .unwrap()
-            .clone();
-
-        let (player_lists, current) = {
-            let data = ctx.data.read().await;
-            let board = data.get::<CurrentIngameBoard>().unwrap();
-            if board.is_none() {
+pub async fn get_data(ctx: &Context) -> Option<(String, Vec<String>, i64, i64)> {
+    let (player_lists, current) = {
+        let data = ctx.data.read().await;
+        let board = data.get::<CurrentIngameBoard>().unwrap();
+        if board.is_none() {
+            return None;
+        }
+        let board = board.as_ref().unwrap().clone();
+        (send_list(data).await, board)
+    };
+    let (players, removes, total) = {
+        let mut smp_players = Vec::new();
+        for line in player_lists {
+            if !line.contains("SMP") {
                 continue;
             }
-            let board = board.as_ref().unwrap().clone();
-            (send_list(data).await, board)
-        };
-        let (adds, removes, total) = {
-            let mut smp_players = Vec::new();
-            for line in player_lists {
-                if !line.contains("SMP") {
-                    continue;
-                }
-                let players = line.split(": ").last().unwrap();
-                for player in players.split(", ") {
-                    if !player.is_empty() {
-                        smp_players.push(player.to_string());
-                    }
+            let players = line.split(": ").last().unwrap();
+            for player in players.split(", ") {
+                if !player.is_empty() {
+                    smp_players.push(player.to_string());
                 }
             }
-            let Some(scoreboard) = get_scoreboard(&ctx, &current).await else {
+        }
+        let Some(scoreboard) = get_scoreboard(&ctx, &current).await else {
+            return None;
+        };
+        let mut removes = 0;
+        for player in &smp_players {
+            let Some(score) = scoreboard.get_score(player) else {
                 continue;
             };
-            let mut removes = 0;
-            for player in &smp_players {
-                let Some(score) = scoreboard.get_score(player) else {
-                    continue;
-                };
-                removes += score;
-            }
-            (smp_players, removes, scoreboard.total)
+            removes += score;
+        }
+        (smp_players, removes, scoreboard.total)
+    };
+    Some((current, players, removes, total))
+}
+
+pub async fn scoreboard_update(ctx: &Context) {
+    let mut data = None;
+    let (tx, _) = ctx
+        .data
+        .read()
+        .await
+        .get::<TaurusChannel>()
+        .unwrap()
+        .clone();
+    let calc = Calc::new(&tx, "SMP").await;
+    let mut i = 0;
+    loop {
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        i += 1;
+        if i % 10 == 0 {
+            data = get_data(ctx).await;
+            i = 0;
+        }
+        let Some((current, players, removes, total)) = &data else {
+            continue;
         };
-        let calc = Calc::new(&tx, "SMP").await;
-        calc.set(total).await;
-        calc.remove(removes).await;
-        for add in adds {
-            calc.add_player(&add, &current).await;
+
+        calc.set(*total).await;
+        calc.remove(*removes).await;
+        for player in players {
+            calc.add_player(&player, &current).await;
         }
         calc.set_other_total(&current).await;
     }
