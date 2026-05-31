57d2823124999ecfa03f1e1ea0783b42a7378666
BlueKossa <dexter@stormdal.se>
2026-06-01 00:48:26 +0200

    fixed command anvil

--- anvil_script/anvil.py
+++ anvil_script/anvil.py
@@ -70,7 +70,7 @@ def peri():
     block_cnt, air_cnt = get_perim("overworld", 1905, 94, -2865)
     percent = block_cnt / air_cnt
     left = air_cnt - block_cnt
-    print(f"Jepstein | ~{percent:.2f} done \\n {left} non-air left")
+    print(f"PRINT Jepstein | ~{percent:.2f} done \\n {left} non-air left")
 
 
 
